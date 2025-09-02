44a57c9d854d1f13fbdff690c70a72bd887676a0
anmol0909 <anmol2002tyagi@gmail.com>
2025-09-02 20:08:23 +0530

    Views for inventory

--- db/migrate/20250901102933_create_members.rb
+++ db/migrate/20250901102933_create_members.rb
@@ -3,7 +3,7 @@ class CreateMembers < ActiveRecord::Migration[7.1]
     create_table :members do |t|
       t.string :name, null: false
       t.string :email
-      t.integer :phone_number
+      t.bigint :phone_number
       t.timestamps
     end
   end
